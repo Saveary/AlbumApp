a3835d2ca5300ae1f5663339be139fdfdee66b0d
Save <savearyj@gmail.com>
2017-12-22 18:16:25 -0500

    Index change

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// Import a library to help create a component 
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
 
+// Create said component (a thing that produces content)
+const App = () => {
+    return (
+        <Text>Random Text</Text>
+    );
+};
+
+// Render (Displey) to the device 
 AppRegistry.registerComponent('Album', () => App);
