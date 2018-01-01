7a742795041311211722e121039a5a6b2e76a533
Save <savearyj@gmail.com>
2017-12-31 19:07:18 -0500

    AlbumList inclusion

---
+++ src/components/AlbumList.js
@@ -0,0 +1,13 @@
+// Boiler plate react native 
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+    return (
+    <View>
+        <Text>Album List!</Text>
+    </View>
+    );
+};
+
+export default AlbumList;
